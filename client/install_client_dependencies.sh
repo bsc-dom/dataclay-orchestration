7293359a9d29485db6c875f3294274e58d0ce3bc
dgasull <dgasull@bsc.es>
2020-04-23 17:50:36 +0200

    Using only singularity and modifying deployment of environments

--- client/install_client_dependencies.sh
+++ client/install_client_dependencies.sh
@@ -1,54 +1,36 @@
 #!/bin/bash
 ### Script intended to obtain everything necessary to run client applications without containers ### 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-EXTRAE_HOME=$SCRIPTDIR/.extrae
-DATACLAY_EXTRAE_WRAPPER_LIB=$SCRIPTDIR/pyclay/pyextrae/dataclay_extrae_wrapper.so
 DATACLAY_HOME=$SCRIPTDIR/..
-
-while test $# -gt 0
-do
-	case "$1" in
-			--extrae) 
-	        	shift
-	        	EXTRAE_HOME=$1
-	        	;;
-			*)  
-				echo "Wrong option $1 provided" 
-				exit 1
-            ;;
-    esac
-    shift
-done
-
-echo "- EXTRAE_HOME = $EXTRAE_HOME"
 
 # sanity check 
 rm -rf $SCRIPTDIR/javaclay/
 rm -rf $SCRIPTDIR/pyclay 
 
-echo "Obtaining javaclay..."
+echo "Installing javaclay..."
 mkdir -p $SCRIPTDIR/javaclay/
 singularity exec $DATACLAY_HOME/singularity/images/logicmodule.sif \
 	cp /home/dataclayusr/dataclay/dataclay.jar $SCRIPTDIR/javaclay/dataclay.jar
-echo "Obtaining pyclay..."
+echo "Installing pyclay..."
 singularity exec $DATACLAY_HOME/singularity/images/dspython.sif \
 	cp -r /home/dataclayusr/dataclay/dataclay_venv $SCRIPTDIR/pyclay/
 
-echo "Obtaining pyextrae..."
-singularity exec $DATACLAY_HOME/singularity/images/dspython.sif \
-	cp -r /home/dataclayusr/dataclay/pyextrae $SCRIPTDIR/pyclay/
-
-singularity exec $DATACLAY_HOME/singularity/images/logicmodule.sif \
-	cp -r /home/dataclayusr/.extrae $EXTRAE_HOME
-
-# Compile dataClay extrae wrapper
-pushd $SCRIPTDIR/pyclay/pyextrae 
-gcc -L${EXTRAE_HOME}/lib -I${EXTRAE_HOME}/include extrae_wrapper.c \
-	-lpttrace -fPIC --shared -o ${DATACLAY_EXTRAE_WRAPPER_LIB}
-
-# Copy extrae configurations 
-singularity exec $DATACLAY_HOME/singularity/images/dspython.sif \
-	cp /home/dataclayusr/dataclay/extrae/extrae_python.xml $SCRIPTDIR/pyclay/pyextrae
-popd
-
-rm -rf $EXTRAE_HOME
+if [ -z $EXTRAE_HOME ]; then 
+	echo "WARNING: EXTRAE_HOME not set. To install extrae necessary libraries for pyClay, make sure extrae is installed. "
+else
+	echo "Using Extrae installed at $EXTRAE_HOME"
+	DATACLAY_EXTRAE_WRAPPER_LIB=$SCRIPTDIR/pyclay/pyextrae/dataclay_extrae_wrapper.so
+	echo "Installing pyclay-extrae..."
+	singularity exec $DATACLAY_HOME/singularity/images/dspython.sif \
+		cp -r /home/dataclayusr/dataclay/pyextrae $SCRIPTDIR/pyclay/
+	
+	# Compile dataClay extrae wrapper
+	pushd $SCRIPTDIR/pyclay/pyextrae 
+	gcc -L${EXTRAE_HOME}/lib -I${EXTRAE_HOME}/include extrae_wrapper.c \
+		-lpttrace -fPIC --shared -o ${DATACLAY_EXTRAE_WRAPPER_LIB}
+	
+	# Copy extrae configurations 
+	singularity exec $DATACLAY_HOME/singularity/images/dspython.sif \
+		cp /home/dataclayusr/dataclay/extrae/extrae_python.xml $SCRIPTDIR/pyclay/pyextrae
+	popd
+fi
