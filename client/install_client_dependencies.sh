56b2cb197fa426608724c2ac1ca4f6fe55bbe314
dgasull <dgasull@bsc.es>
2020-05-02 14:32:51 +0200

    Added singularity pull + modified paths, ld library paths
    Some paths require to be mounted as files in singularity like
    LD_LIBRARY_PATH

--- client/install_client_dependencies.sh
+++ client/install_client_dependencies.sh
@@ -11,9 +11,20 @@ echo "Installing javaclay..."
 mkdir -p $SCRIPTDIR/javaclay/
 singularity exec $DATACLAY_HOME/singularity/images/logicmodule.sif \
 	cp /home/dataclayusr/dataclay/dataclay.jar $SCRIPTDIR/javaclay/dataclay.jar
+mkdir -p $SCRIPTDIR/javaclay/aspectj/
+singularity exec $DATACLAY_HOME/singularity/images/logicmodule.sif \
+	cp /usr/share/java/aspectjrt.jar $SCRIPTDIR/javaclay/aspectj/
+singularity exec $DATACLAY_HOME/singularity/images/logicmodule.sif \
+	cp /usr/share/java/aspectjweaver.jar $SCRIPTDIR/javaclay/aspectj/
+singularity exec $DATACLAY_HOME/singularity/images/logicmodule.sif \
+	cp /usr/share/java/aspectjtools.jar $SCRIPTDIR/javaclay/aspectj/
+	
 echo "Installing pyclay..."
-singularity exec $DATACLAY_HOME/singularity/images/dspython.sif \
-	cp -r /home/dataclayusr/dataclay/dataclay_venv $SCRIPTDIR/pyclay/
+pyclay3.6
+pip install dataclay <= login0? 
+pyclay3.7
+pyclay3.8
+PYCLAY_SRC=
 
 if [ -z $EXTRAE_HOME ]; then 
 	echo "WARNING: EXTRAE_HOME not set. To install extrae necessary libraries for pyClay, make sure extrae is installed. "
---
+++ singularity/singularity_pull.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#=== FUNCTION ================================================================
+# NAME: get_container_version
+# DESCRIPTION: Get container version
+# PARAMETER 1: Execution environment version i.e. can be python py3.6 or jdk8
+#===============================================================================
+function get_container_version { 
+	EE_VERSION=$1
+	DATACLAY_EE_VERSION="${EE_VERSION//./}"
+	if [[ $DATACLAY_VERSION == *".dev"* ]]; then
+  		VERSION_WITHOUT_DEV="${DATACLAY_VERSION//.dev/}"
+		DATACLAY_CONTAINER_VERSION="${VERSION_WITHOUT_DEV}.${DATACLAY_EE_VERSION}.dev"
+  	else 
+  		DATACLAY_CONTAINER_VERSION="$DATACLAY_VERSION.${DATACLAY_EE_VERSION}"
+	fi
+	echo ${DATACLAY_CONTAINER_VERSION}
+}
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+ORCHESTRATION_DIR=$SCRIPTDIR/..
+source $SCRIPTDIR/PLATFORMS.txt
+DATACLAY_VERSION=$(cat ${ORCHESTRATION_DIR}/VERSION.txt)
+LOCAL_REPOSITORY=$ORCHESTRATION_DIR/singularity/images
+
+for JAVA_VERSION in ${SUPPORTED_JAVA_VERSIONS[@]}; do
+	EXECUTION_ENVIRONMENT_TAG="$(get_container_version jdk$JAVA_VERSION)"
+	singularity pull $LOCAL_REPOSITORY/logicmodule:${EXECUTION_ENVIRONMENT_TAG}.sif library://support-dataclay/default/logicmodule:$EXECUTION_ENVIRONMENT_TAG
+	singularity pull $LOCAL_REPOSITORY/dsjava:${EXECUTION_ENVIRONMENT_TAG}.sif library://support-dataclay/default/dsjava:$EXECUTION_ENVIRONMENT_TAG
+
+done
+for PYTHON_VERSION in ${SUPPORTED_PYTHON_VERSIONS[@]}; do
+	EXECUTION_ENVIRONMENT_TAG="$(get_container_version py$PYTHON_VERSION)"
+	singularity pull $LOCAL_REPOSITORY/dspython:${EXECUTION_ENVIRONMENT_TAG}.sif library://support-dataclay/default/dspython:$EXECUTION_ENVIRONMENT_TAG
+done
+
+singularity pull $LOCAL_REPOSITORY/client:${DATACLAY_VERSION}.sif library://support-dataclay/default/client:${DATACLAY_VERSION}
+
+exit 0
