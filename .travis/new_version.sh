95b43f2e8de7606083d2ec046d06f8329a88f404
dgasull <dgasull@bsc.es>
2020-06-16 16:56:30 +0200

    Releasing 2.4

--- .travis/new_version.sh
+++ .travis/new_version.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_BRANCH" == "master" ]; then 
      export VERSION=$(cat VERSION.txt);
      NEW_VERSION=$(echo "$VERSION + 0.1" | bc)
-     echo ${NEW_VERSION}.dev > VERSION.txt
+     echo ${NEW_VERSION} > VERSION.txt
      git add VERSION.txt
 	 git commit -m "New release"
 	 git push origin HEAD:$TRAVIS_BRANCH
@@ -11,6 +11,7 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 	 ## update develop branch also ##
 	 git fetch
 	 git checkout develop
+     echo ${NEW_VERSION}.dev > VERSION.txt
      git add VERSION.txt
 	 git commit -m "New release"
 	 git push origin HEAD:develop
