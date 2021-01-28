30fb251b94b918bf055a10e571847b216b908b8e
dgasull <dgasull@bsc.es>
2021-01-28 15:34:20 +0100

    Added initializer image + Updating submodules

--- .travis/git_tag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_BRANCH" == "master" ]; then 
-     export GIT_TAG=$(cat VERSION.txt)
-     git tag $GIT_TAG -a -m "New tag"
-     git push origin $GIT_TAG
-else
-     echo "Skipping Git tag because current branch is not master";
-fi
